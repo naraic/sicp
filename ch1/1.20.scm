72b29de1ac8f98a1c6efef850d02db5b9d0f6416
naraic <naraic0o@gmail.com>
2018-03-14 10:22:01 +0000

    1.20 complete

--- ch1/1.20.scm
+++ ch1/1.20.scm
@@ -13,8 +13,18 @@
 
 ;normal-order
 ;using the weird if
+;also gonna use 'r' for remainder to save space
 
 (gcd 206 40)
-(gcd 40 (remainder 206 40))
-(gcd 40 (gcd (remainder 40 (remainder 206 40))) ;+1 for the if
-(gcd 40 (gcd (remainder 40 (remainder 206 40))) ;+1 for the if
+(gcd 40 (r 206 40))     ;+1 call in if
+(gcd (r 206 40)
+     (r 40 (r 206 40))) ;+2 calls in if
+(gcd (r 40 (r 206 40))
+     (r (r 206 40) (r 40 (r 206 40)))) ;+4 calls in if
+(gcd (r (r 206 40) (r 40 (r 206 40)))
+     (r (r 40 (r 206 40)) (r (r 206 40) (r 40 (r 206 40))))) ;+7 calls in if
+
+; b evaluates to 0 here, so a is the result, which contains 4 remainders
+; total is 4 + 7 + 4 + 2 + 1 calls = 18 total
+    
+
