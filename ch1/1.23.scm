638cf318780177e85a8b84fc827ae7748320e4e8
naraic <naraic0o@gmail.com>
2018-03-23 14:02:47 +0000

    fixed 22 & 23

--- ch1/1.23.scm
+++ ch1/1.23.scm
@@ -1,9 +1,15 @@
 (define (smallest-divisor n)
   (find-divisor n 2))
+
 (define (find-divisor n test-divisor)
+  (define (next divisor)
+    (if (= 2 divisor) 
+        3
+        (+ 2 divisor)))
   (cond ((> (square test-divisor) n) n)
 	((divides? test-divisor n) test-divisor)
-	(else (find-divisor n (+ test-divisor 1)))))
+	(else (find-divisor n (next test-divisor)))))
+
 (define (divides? a b)
   (= (remainder b a) 0))
 
@@ -14,9 +20,11 @@
   (newline)
   (display n)
   (start-prime-test n (runtime)))
+
 (define (start-prime-test n start-time)
   (if (prime? n)
       (report-prime (- (runtime) start-time))))
+
 (define (report-prime elapsed-time)
   (display " *** ")
   (display elapsed-time))
@@ -26,12 +34,16 @@
 	((even? start) (timed-prime-test start)(search-for-primes (+ 1 start) end))
 	(else (timed-prime-test start)(search-for-primes (+ 2 start) end))))
 
-;(search-for-primes 1 10)
+(search-for-primes 1 10)
 
 ;(timed-prime-test 100)
 
-
+(define (next n)
+  (if (= n 2)
+      3
+      (+ n 2)))
  
+
       
       
  
